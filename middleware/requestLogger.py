826e8ea769b104e850c711d88acb87e7745bbb16
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-03-31 14:41:31 +0700

    Delete middleware directory

--- middleware/requestLogger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from flask import request
-
-def log_request():
-    now = datetime.datetime.now().isoformat()
-    method = request.method
-    path = request.path
-    ip = request.remote_addr
-    print(f"[{now}] {ip} {method} {path}")
