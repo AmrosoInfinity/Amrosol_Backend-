00e36a953bfd90ecc7809ef2d09078ebab6a0b6f
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-03-31 10:42:30 +0700

    Update requestLogger.js

--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -1,6 +1,11 @@
-function requestLogger(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next();
-}
+import datetime
 
-module.exports = requestLogger;
+def request_logger():
+    now = datetime.datetime.now().isoformat()
+    # Flask menyediakan objek request global
+    from flask import request
+    method = request.method
+    path = request.path
+    ip = request.remote_addr
+
+    print(f"[{now}] {ip} {method} {path}")
