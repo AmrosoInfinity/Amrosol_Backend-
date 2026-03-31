c57e6e753e88b6a2c5a360440d49739c2b42add8
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-03-31 11:46:05 +0700

    Update tokenController.py

--- controllers/tokenController.py
+++ controllers/tokenController.py
@@ -1,4 +1,14 @@
 from services.token_cache import fetch_token
 
 def get_token(service, userId, hashToken):
-    return fetch_token(service, userId, hashToken)
+    token_data = fetch_token(service, userId, hashToken)
+    if token_data:
+        return {
+            "service": service,
+            "userId": userId,
+            "hashToken": hashToken,
+            "token": token_data["token"],
+            "ttl": f"{token_data['ttl']} detik",
+            "expire_at": token_data["expire_at"]
+        }
+    return None
