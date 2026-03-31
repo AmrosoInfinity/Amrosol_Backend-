ec2e43a8024d9d515afb4aba3579dc57d55cf4e6
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-03-31 11:46:53 +0700

    Update cacheConfig.py

--- config/cacheConfig.py
+++ config/cacheConfig.py
@@ -1,21 +1,5 @@
-import time
-
-cache = {}
-
-def set_token(service, userId, hashToken, ttl=60):
-    expire_at = time.time() + ttl
-    cache[(service, userId, hashToken)] = {
-        "token": hashToken,
-        "ttl": ttl,
-        "expire_at": expire_at
-    }
-
-def fetch_token(service, userId, hashToken):
-    key = (service, userId, hashToken)
-    if key in cache:
-        data = cache[key]
-        if time.time() < data["expire_at"]:
-            return data
-        else:
-            del cache[key]
-    return None
+TTL_DEFAULT = 60
+TTL_SERVICE = {
+    "grab": 60,
+    "gojek": 120
+}
